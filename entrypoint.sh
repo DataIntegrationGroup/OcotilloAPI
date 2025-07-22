72144ea634a3059f8192d6f09d538c625c04ed7b
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-07-22 13:49:49 -0600

    feat: autoreload changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ echo "PostgreSQL is ready!"
 echo "Applying migrations..."
 alembic upgrade head
 echo "Starting the application..."
-uvicorn main:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
