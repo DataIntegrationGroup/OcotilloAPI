44cfa0589fd9d9e7e677dbb95ea8848783037014
Chase Martin <chasemartin@macbook-air.lan>
2025-07-23 17:58:02 -0700

    feat: adds location patch test and endpoint

--- api/location.py
+++ api/location.py
@@ -25,11 +25,11 @@ from constants import SRID_WGS84
 from core.dependencies import session_dependency
 from db import adder, Location, WellThing
 from db.engine import get_db_session
-from schemas_v2.location import CreateLocation, LocationResponse
+from schemas_v2.location import CreateLocation, LocationResponse, UpdateLocation
 from schemas_v2.thing import LocationWellResponse
 from services.geospatial_helper import make_within_wkt
 from services.query_helper import make_query
-
+from services.crud_helper import model_patcher
 
 from fastapi import APIRouter
 
@@ -50,6 +50,19 @@ def create_location(
     """
     return adder(session, Location, location_data)
 
+@router.patch(
+    "/{location_id}",
+    summary="Update a location",
+)
+def update_location(
+    location_id: int,
+    location_data: UpdateLocation,
+    session: Session = Depends(get_db_session),
+) -> LocationResponse:
+    """
+    Update a sample location in the database.
+    """
+    return model_patcher(session, Location, location_id, location_data)
 
 # @router.get("/shapefile", summary="Get location as shapefile")
 # async def get_location_shapefile(
