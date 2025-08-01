e1a3df5d00735feb34d1199f1d4cdbe726265292
ksmuczynski <ksmuczynski@users.noreply.github.com>
2025-08-01 20:36:36 +0000

    Formatting changes

--- db/sample.py
+++ db/sample.py
@@ -27,49 +27,58 @@ from typing import List, Optional
 
 import datetime
 
+
 class Sample(Base, AutoBaseMixin):
     """
-        Defines the Sample table, which stores data for individual
-        sampling events.
-        """
+    Defines the Sample table, which stores data for individual
+    sampling events.
+    """
+
     # __table_name__ is inherited from AutoBaseMixin.
 
     # --- Column Definitions ---
     # Foreign Keys
     thing_id: Mapped[int] = mapped_column(
         ForeignKey("thing.id", ondelete="CASCADE"),
-        nullable = False,
-        comment="Foreign key to the Thing (e.g., sampling location) table.")
+        nullable=False,
+        comment="Foreign key to the Thing (e.g., sampling location) table.",
+    )
     sensor_id: Mapped[Optional[int]] = mapped_column(
         ForeignKey("sensor.id"),
         unique=True,
-        comment="Foreign key for the specific equipment used.")
+        comment="Foreign key for the specific equipment used.",
+    )
 
     # Sample Attributes
     sample_date: Mapped[datetime.datetime] = mapped_column(
-        DateTime,
-        comment="Date and time of sample collection.")
+        DateTime, comment="Date and time of sample collection."
+    )
     sample_matrix: Mapped[Optional[str]] = mapped_column(
-        comment="The material of the sample (e.g., 'gw', 'soil').")
+        comment="The material of the sample (e.g., 'gw', 'soil')."
+    )
     sample_method: Mapped[Optional[str]] = mapped_column(
-        comment="Method used to collect the sample.")
+        comment="Method used to collect the sample."
+    )
     field_sample_id: Mapped[str] = mapped_column(
-        unique=True,
-        comment="User-defined ID for field tracking.")
+        unique=True, comment="User-defined ID for field tracking."
+    )
     sampler_name: Mapped[Optional[str]] = mapped_column(
-        comment="Name of the person who collected the sample.")
+        comment="Name of the person who collected the sample."
+    )
     qc_sample: Mapped[str] = mapped_column(
-        default='Original',
-        comment="Quality control sample type (e.g., 'Original', 'field dupe').")
+        default="Original",
+        comment="Quality control sample type (e.g., 'Original', 'field dupe').",
+    )
     sample_top: Mapped[Optional[float]] = mapped_column(
-        Float,
-        comment="Top depth of a discrete sample interval.")
+        Float, comment="Top depth of a discrete sample interval."
+    )
     sample_bottom: Mapped[Optional[float]] = mapped_column(
-        Float,
-        comment="Bottom depth of a discrete sample interval.")
+        Float, comment="Bottom depth of a discrete sample interval."
+    )
     duplicate_sample_number: Mapped[int] = mapped_column(
         default=0,
-        comment="Identifier for duplicate samples (0 = original sample, not a duplicate, 1 = dup no.1, 2 = dup no.2, etc.).")
+        comment="Identifier for duplicate samples (0 = original sample, not a duplicate, 1 = dup no.1, 2 = dup no.2, etc.).",
+    )
 
     # --- Relationship Definitions ---
     thing: Mapped["Thing"] = relationship(back_populates="samples")
@@ -80,13 +89,19 @@ class Sample(Base, AutoBaseMixin):
     # sample_top, sample_bottom, duplicate_sample, field_sample_id, and qc_sample fields.
     __table_args__ = (
         UniqueConstraint(
-            'thing_id', 'sample_date', 'sample_matrix', 'sample_top',
-            'sample_bottom', 'duplicate_sample_number', 'field_sample_id', 'qc_sample',
-            name='uix_sample_uniqueness'
+            "thing_id",
+            "sample_date",
+            "sample_matrix",
+            "sample_top",
+            "sample_bottom",
+            "duplicate_sample_number",
+            "field_sample_id",
+            "qc_sample",
+            name="uix_sample_uniqueness",
         ),
     )
 
-    #---Jake original code---
+    # ---Jake original code---
     # collection_timestamp = mapped_column(DateTime, nullable=False)
     # collection_method = lexicon_term(nullable=False)
     #
