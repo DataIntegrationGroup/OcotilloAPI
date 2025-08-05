bd8cbe04b6e0ea524585080b8e2102f855b24597
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-08-04 18:21:51 -0600

    feat: add new schema model fields and inherit field validators
    POST and PATCH requests require the same field validators, so we
    inherit the validators from a base model class for both request types.

--- schemas_v2/sample.py
+++ schemas_v2/sample.py
@@ -17,23 +17,57 @@ from datetime import datetime, timezone
 from pydantic import BaseModel, field_validator
 
 from db.engine import session_ctx
-from db import Thing
+from db import Thing, Sensor, Sample
+
+
+"""
+REFACTOR TODO: can we use inheritance for commonly defined fields and then set them as optional 
+or not between Create, Update, and Response schemas?
+"""
 
 
 # -------- VALIDATE ----------
-
-
-class SampleValidator(BaseModel):
+class ValidateSample(BaseModel):
     """
-    Validator for Sample data.
+    Validator for Sample data for Create and Update schemas.
     """
+
+    @field_validator("field_sample_id", check_fields=False)
+    def validate_field_sample_id(cls, field_sample_id: str) -> str:
+        """
+        Validate that the field_sample_id is unique.
+        """
+        if field_sample_id is not None:
+            with session_ctx() as session:
+                existing_sample = (
+                    session.query(Sample)
+                    .filter_by(field_sample_id=field_sample_id)
+                    .first()
+                )
+                if existing_sample:
+                    raise ValueError(
+                        f"Field sample ID {field_sample_id} already exists."
+                    )
+        return field_sample_id
+
+    @field_validator("sensor_id", check_fields=False)
+    def validate_sensor_id(cls, sensor_id: int | None) -> int | None:
+        """
+        Validate that the sensor_idexists in the database.
+        """
+        if sensor_id is not None:
+            with session_ctx() as session:
+                sensor = session.get(Sensor, sensor_id)
+                if not sensor:
+                    raise ValueError(f"Sensor with ID {sensor_id} does not exist.")
+        return sensor_id
 
     @field_validator("thing_id", check_fields=False)
-    def validate_thing_id_exists(cls, thing_id: int) -> int:
+    def validate_thing_id_exists(cls, thing_id: int | None) -> int | None:
         """
         Validate that the thing_id exists in the database.
         """
-        if thing_id:
+        if thing_id is not None:
             with session_ctx() as session:
                 thing = session.get(Thing, thing_id)
                 if not thing:
@@ -41,68 +75,121 @@ class SampleValidator(BaseModel):
         return thing_id
 
     @field_validator("sample_date", check_fields=False)
-    def validate_sample_date(cls, sample_date: datetime) -> datetime:
+    def validate_sample_date(cls, sample_date: datetime | None) -> datetime | None:
         """
         Validate that the sample_date is not in the future.
         """
-        if sample_date:
-            if sample_date:
-                if sample_date > datetime.now(tz=timezone.utc):
-                    raise ValueError(
-                        f"Sample date {sample_date} cannot be in the future."
-                    )
+        if sample_date is not None:
+            if sample_date > datetime.now(tz=timezone.utc):
+                raise ValueError(f"Sample date {sample_date} cannot be in the future.")
         return sample_date
+
+    # # REFACTOR TODO: is below ground negative or positive? the combine this with validate_sample_bottom defined below
+    # @field_validator("sample_bottom", check_fields=False)
+    # def validate_sample_bottom(cls, sample_bottom: float | None, values) -> float | None:
+    #     """
+    #     Validate that the sample_bottom is not less than sample_top.
+    #     """
+    #     sample_top = values.get('sample_top')
+    #     if sample_bottom is not None and sample_top is not None:
+    #         if sample_bottom > sample_top:
+    #             raise ValueError(
+    #                 "Sample bottom cannot be greater than sample top."
+    #             )
+    #     return sample_bottom
+
+    @field_validator("sample_bottom", check_fields=False)
+    def validate_sample_bottom(
+        cls, sample_bottom: float | None, values: dict
+    ) -> float | None:
+        """
+        Validate that the sample_bottom is definfed if sample_top is defined
+        """
+        sample_top = values.get("sample_top")
+        if sample_bottom is not None and sample_top is None:
+            raise ValueError("Sample bottom must be defined if sample top is defined.")
+        elif sample_bottom is None and sample_top is not None:
+            raise ValueError("Sample bottom must be defined if sample top is defined.")
+        return sample_bottom
 
 
 # -------- CREATE ----------
-class CreateSample(SampleValidator):
+class CreateSample(ValidateSample):
     thing_id: int
     sample_type: str
     field_sample_id: str
+    sample_date: datetime
+    release_status: str
+    sampler_name: str  # REFACTOR TODO: update with enum/restricted values
+    qc_sample: str = "Original"
 
     sensor_id: int | None = None
-    sample_date: datetime | None = None
-    sample_matrix: str | None = None
-    sample_method: str | None = None
-    sampler_name: str | None = None
-    qc_sample: str | None = None
-    duplicate_sample_number: int | None = None
+    sample_matrix: str | None = (
+        None  # REFACTOR TODO: update with enum/restricted values
+    )
+    sample_method: str | None = (
+        None  # REFACTOR TODO: update with enum/restricted values
+    )
 
+    duplicate_sample_number: int | None = 0
+
+    # REFACTOR TODO: update with numeric restrictions? Are negative values below ground and positive above?
+    # for example: wells below, rain above, and soil/rock could be at ground surface
     sample_top: float | None = None
     sample_bottom: float | None = None
 
-    release_status: str
-
-
-class CreateGeochemicalSample(BaseModel):
-    """
-    Represents a geochemical sample in the collaborative network.
-    """
-
-    sample_id: int
-
-
-class CreateGeothermalSample(BaseModel):
-    """
-    Represents a geothermal sample in the collaborative network.
-    """
-
-    sample_id: int
-
 
 # -------- UPDATE ----------
-class UpdateSample(SampleValidator):
-    sample_date: datetime | None = None
-    sample_method: str | None = None
-    thing_id: int | None = None
+class UpdateSample(ValidateSample):
+    """
+    Development notes:
+
+    setting <type> = None makes the field optional, but if it is defined it must be of that type.
+    """
+
+    thing_id: int = None  # REFACTOR TODO: should users be able to change this?
+    sample_type: str = None
+    field_sample_id: str = None
+    sample_date: datetime = None
+    release_status: str = None
+    sampler_name: str = None  # REFACTOR TODO: update with enum/restricted values
+    qc_sample: str = None
+
+    sensor_id: int | None = None  # REFACTOR TODO: should users be able to change this?
+    sample_matrix: str | None = (
+        None  # REFACTOR TODO: update with enum/restricted values
+    )
+    sample_method: str | None = (
+        None  # REFACTOR TODO: update with enum/restricted values
+    )
+
+    duplicate_sample_number: int | None = None
+
+    # REFACTOR TODO: update with numeric restrictions? Are negative values below ground and positive above?
+    # for example: wells below, rain above, and soil/rock could be at ground surface
+    sample_top: float | None = None
+    sample_bottom: float | None = None
 
 
 # -------- RESPONSE ----------
 class SampleResponse(BaseModel):
     id: int
-    sample_date: datetime
-    sample_method: str
     thing_id: int
+    sample_type: str
+    field_sample_id: str
+    sample_date: datetime
+    release_status: str
+    sampler_name: str
+    qc_sample: str
+
+    sensor_id: int | None
+    sample_matrix: str | None
+    sample_method: str | None
+
+    duplicate_sample_number: int | None
+
+    sample_top: float | None
+    sample_bottom: float | None
 
 
 # ============= EOF =============================================
