0c3c167d5533f8b746e2b3e1000d294d26ae8281
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-08-12 15:39:12 -0600

    refactor: use fixture in existing tests

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -13,50 +13,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
-from tests import client
+from db import Sensor
+from tests import client, cleanup_post_test
+
+# ====== POST tests ============================================================
 
 
 def test_add_sensor():
-    response = client.post(
-        "/sensor",
-        json={
-            "name": "Test Sensor",
-            "model": "Model X",
-            "serial_no": "123456",
-            "date_installed": "2023-01-01T00:00:00",
-            # "date_removed": None,
-            "recording_interval": 60,
-            "notes": "Test equipment",
-            # "location_id": 2,
-        },
-    )
+    payload = {
+        "name": "Test Sensor 2",
+        "model": "Model X",
+        "serial_no": "12345678",
+        "datetime_installed": "2024-01-01T00:00:00Z",
+        "datetime_removed": None,
+        "recording_interval": 60,
+        "notes": "Test equipment",
+    }
+    response = client.post("/sensor", json=payload)
     assert response.status_code == 201
     data = response.json()
     assert "id" in data
-    # assert data["location_id"] == 2
+    assert data["name"] == payload["name"]
+    assert data["model"] == payload["model"]
+    assert data["serial_no"] == payload["serial_no"]
+    assert data["datetime_installed"] == payload["datetime_installed"]
+    assert data["datetime_removed"] == payload["datetime_removed"]
+    assert data["recording_interval"] == payload["recording_interval"]
+    assert data["notes"] == payload["notes"]
+
+    # cleanup after post test
+    cleanup_post_test(Sensor, data["id"])
 
 
-def test_get_sensors():
+# ====== GET tests =============================================================
+
+
+def test_get_sensors(sensor):
     response = client.get("/sensor")
     assert response.status_code == 200
     data = response.json()
-    # assert isinstance(items, list), "Expected a list of sensors"
-    assert "items" in data
-    items = data["items"]
-    assert "id" in items[0], "Expected 'id' in sensor items"
-    # assert "name" in items[0], "Expected 'name' in sensor items"
-    # assert "equipment_type" in items[0], "Expected 'equipment_type' in sensor items"
+    assert data["total"] == 1
+    assert data["items"][0]["id"] == sensor.id
+    assert data["items"][0]["name"] == sensor.name
+    assert data["items"][0]["model"] == sensor.model
+    assert data["items"][0]["serial_no"] == sensor.serial_no
+    assert data["items"][0]["datetime_installed"] == sensor.datetime_installed
+    assert data["items"][0]["datetime_removed"] == sensor.datetime_removed
+    assert data["items"][0]["recording_interval"] == sensor.recording_interval
+    assert data["items"][0]["notes"] == sensor.notes
 
 
-def test_get_sensor():
-    # Assuming the first sensor has an ID of 1
-    response = client.get("/sensor/1")
+def test_get_sensor_by_id(sensor):
+    response = client.get(f"/sensor/{sensor.id}")
     assert response.status_code == 200
     data = response.json()
-    assert "id" in data, "Expected 'id' in sensor data"
-    assert data["id"] == 1, "Expected sensor ID to be 1"
-    assert "name" in data, "Expected 'name' in sensor data"
-    # assert "equipment_type" in data, "Expected 'equipment_type' in sensor data"
+    assert data["id"] == sensor.id, "Expected sensor ID to match"
+    assert data["name"] == sensor.name, "Expected sensor name to match"
+    assert data["model"] == sensor.model, "Expected sensor model to match"
+    assert data["serial_no"] == sensor.serial_no, "Expected sensor serial_no to match"
+    assert (
+        data["datetime_installed"] == sensor.datetime_installed
+    ), "Expected sensor datetime_installed to match"
+    assert (
+        data["datetime_removed"] == sensor.datetime_removed
+    ), "Expected sensor datetime_removed to match"
+    assert (
+        data["recording_interval"] == sensor.recording_interval
+    ), "Expected sensor recording_interval to match"
+    assert data["notes"] == sensor.notes, "Expected sensor notes to match"
 
 
 # ============= EOF =============================================
