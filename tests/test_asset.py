a3235489d5d1597c7cbd2b5d0da173dd77e42f39
jakeross <jirhiker@gmail.com>
2025-08-18 11:25:10 -0600

    fix: fixed asset tests

--- tests/test_asset.py
+++ tests/test_asset.py
@@ -30,6 +30,8 @@ class MockStorageBucket:
     def blob(self, *args, **kwargs):
         return MockBlob()
 
+    def get_blob(self, *args, **kwargs):
+        return None
 
 def mock_storage_bucket():
     return MockStorageBucket()
@@ -105,7 +107,7 @@ def test_add_asset_with_label(thing):
             "label": "Test Asset",
             "url": "https://storage.googleapis.com/mock-bucket/mock-asset",
             "storage_service": "mock_service",
-            "storage_path": "mock/path/to/asset",
+            "storage_path": "mock/path/to/asset/test_asset.png",
             "mime_type": "image/png",
             "size": 12345,
         },
