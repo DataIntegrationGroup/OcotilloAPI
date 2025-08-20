7435f60684f3c14d8f6acd789f367c7af481a11a
jakeross <jirhiker@gmail.com>
2025-08-20 10:58:03 -0600

    refactor: signed_url optional in AssetResponse

--- schemas/asset.py
+++ schemas/asset.py
@@ -43,7 +43,7 @@ class AssetResponse(BaseAsset):
     created_at: AwareDatetime
     storage_service: str
     uri: str
-    signed_url: str
+    signed_url: str | None = None
 
 
 # -------- UPDATE ----------
