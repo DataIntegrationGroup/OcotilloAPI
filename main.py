3ff3cca28ca9b9d46e1ae07600f7035fa5df3514
jakeross <jirhiker@gmail.com>
2025-08-21 16:27:00 -0600

    feat: integrate Sentry for error tracking and performance monitoring

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import os
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+    send_default_pii=True,
+)
+
 
 from fastapi_pagination import add_pagination
 
