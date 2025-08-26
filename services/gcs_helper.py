5285228c933a7437ea23dfe0605ad45004405587
jakeross <jirhiker@gmail.com>
2025-08-25 18:01:22 -0600

    fix: add debug prints for GCS_SERVICE_ACCOUNT_KEY in production mode

--- services/gcs_helper.py
+++ services/gcs_helper.py
@@ -33,6 +33,8 @@ def get_storage_bucket() -> storage.Bucket:
 
     if settings.mode == "production":
         key_json = os.environ.get("GCS_SERVICE_ACCOUNT_KEY")
+        print(key_json)
+        print(json.loads(key_json, strict=False))
         # Load service account credentials
         creds = service_account.Credentials.from_service_account_info(
             json.loads(key_json)
