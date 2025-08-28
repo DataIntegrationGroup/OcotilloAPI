35fc7aae121ebdcfe867c95479a70d1655155db9
jakeross <jirhiker@gmail.com>
2025-08-28 09:50:40 -0600

    feat: implement group transfer functionality and add projects CSV

--- db/thing.py
+++ db/thing.py
@@ -117,6 +117,17 @@ class WellScreen(Base, AutoBaseMixin):
     # Define a relationship to well if needed
     thing = relationship("Thing")
 
+# TODO: this could be the model used to handle AMP monitoring
+# class FieldSamplingAdministation(Base, AutoBaseMixin):
+#     # the thing being monitored
+#     thing_id: Mapped[int] = mapped_column(Integer, ForeignKey("thing.id", ondelete="CASCADE"), nullable=False)
+#
+#     monitoring_frequency: Mapped[str] = mapped_column(lexicon_term(), nullable=False)
+#     well_logger_ok: Mapped[bool] = mapped_column(Boolean, nullable=False)
+#     monitor_ok: Mapped[bool] = mapped_column(Boolean, nullable=False)
+#     sample_ok: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+
 
 # TODO: this could be the model used to handle AMP monitoring
 # class FieldSamplingAdministation(Base, AutoBaseMixin):
