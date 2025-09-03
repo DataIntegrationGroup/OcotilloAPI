0689d773f6176a42e165a7d2dbbafd2d722697e0
jirhiker <jirhiker@users.noreply.github.com>
2025-09-03 17:39:17 +0000

    Formatting changes

--- transfers/util.py
+++ transfers/util.py
@@ -37,7 +37,6 @@ def read_csv(name: str) -> pd.DataFrame:
 def read_csv(name: str) -> pd.DataFrame:
     # TODO: grab csv from storage bucket instead of local directory
 
-
     p = Path(".") / "data" / name
     return pd.read_csv(p)
 
