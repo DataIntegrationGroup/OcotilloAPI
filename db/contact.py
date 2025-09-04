5dcb5cb416cd1473b12c668c0958862c164a2e1b
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-09-04 17:02:56 -0600

    refactor: redo annotations for mapped columns

--- db/contact.py
+++ db/contact.py
@@ -24,10 +24,10 @@ from db.base import Base, AutoBaseMixin, ReleaseMixin, lexicon_term
 
 class ThingContactAssociation(Base, AutoBaseMixin):
     thing_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("thing.id", ondelete="CASCADE")
+        ForeignKey("thing.id", ondelete="CASCADE"), nullable=False
     )
     contact_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("contact.id", ondelete="CASCADE")
+        ForeignKey("contact.id", ondelete="CASCADE"), nullable=False
     )
 
     contact: Mapped[List["Contact"]] = relationship("Contact")
@@ -35,11 +35,11 @@ class ThingContactAssociation(Base, AutoBaseMixin):
 
 
 class Contact(Base, AutoBaseMixin, ReleaseMixin):
-    name: Mapped[str | None] = mapped_column(String(100))
-    organization: Mapped[str | None] = mapped_column(String(100))
-    role: Mapped[str] = lexicon_term()
-    contact_type: Mapped[str] = lexicon_term()
-    nma_pk_owners: Mapped[str | None] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(100), nullable=True)
+    organization: Mapped[str] = mapped_column(String(100), nullable=True)
+    role: Mapped[str] = lexicon_term(nullable=False)
+    contact_type: Mapped[str] = lexicon_term(nullable=False)
+    nma_pk_owners: Mapped[str] = mapped_column(String(100), nullable=True)
 
     phones: Mapped[List["Phone"]] = relationship(
         "Phone", back_populates="contact", passive_deletes=True
@@ -74,10 +74,10 @@ class Contact(Base, AutoBaseMixin, ReleaseMixin):
 
 class Phone(Base, AutoBaseMixin, ReleaseMixin):
     contact_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("contact.id", ondelete="CASCADE")
+        ForeignKey("contact.id", ondelete="CASCADE"), nullable=False
     )
-    phone_number: Mapped[str] = mapped_column(String(20))
-    phone_type: Mapped[str] = lexicon_term()
+    phone_number: Mapped[str] = mapped_column(String(20), nullable=False)
+    phone_type: Mapped[str] = lexicon_term(nullable=False)
 
     contact: Mapped["Contact"] = relationship(
         "Contact", back_populates="phones", passive_deletes=True
@@ -87,10 +87,10 @@ class Phone(Base, AutoBaseMixin, ReleaseMixin):
 
 class Email(Base, AutoBaseMixin, ReleaseMixin):
     contact_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("contact.id", ondelete="CASCADE")
+        Integer, ForeignKey("contact.id", ondelete="CASCADE"), nullable=False
     )
-    email: Mapped[str] = mapped_column(String(100))
-    email_type: Mapped[str] = lexicon_term()
+    email: Mapped[str] = mapped_column(String(100), nullable=False)
+    email_type: Mapped[str] = lexicon_term(nullable=False)
 
     contact: Mapped["Contact"] = relationship(
         "Contact", back_populates="emails", passive_deletes=True
@@ -101,15 +101,15 @@ class Email(Base, AutoBaseMixin, ReleaseMixin):
 
 class Address(Base, AutoBaseMixin, ReleaseMixin):
     contact_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("contact.id", ondelete="CASCADE")
+        ForeignKey("contact.id", ondelete="CASCADE"), nullable=False
     )
-    address_line_1: Mapped[str] = mapped_column(String(255))
-    address_line_2: Mapped[str | None] = mapped_column(String(255))
-    city: Mapped[str] = mapped_column(String(100))
-    state: Mapped[str] = mapped_column(String(50))
-    postal_code: Mapped[str] = mapped_column(String(20))
-    country: Mapped[str] = lexicon_term(default="United States")
-    address_type: Mapped[str] = lexicon_term()
+    address_line_1: Mapped[str] = mapped_column(String(255), nullable=False)
+    address_line_2: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    city: Mapped[str] = mapped_column(String(100), nullable=False)
+    state: Mapped[str] = mapped_column(String(50), nullable=False)
+    postal_code: Mapped[str] = mapped_column(String(20), nullable=False)
+    country: Mapped[str] = lexicon_term(default="United States", nullable=False)
+    address_type: Mapped[str] = lexicon_term(nullable=False)
 
     contact: Mapped["Contact"] = relationship(
         "Contact", back_populates="addresses", passive_deletes=True
