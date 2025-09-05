764e39f7e8c4ea164c35fd339a74e4501bea0599
jakeross <jirhiker@gmail.com>
2025-09-05 00:45:32 -0600

    feat: update health check endpoint to use POST method with JSON payload

--- transfer_test.sh
+++ transfer_test.sh
@@ -1,2 +1,4 @@
-curl -X GEt "https://nma-ocotillo-transfer-95715287188.us-central1.run.app/wells" \
--H "Authorization: bearer $(gcloud auth print-identity-token)"
+curl -X POST "https://nma-ocotillo-transfer-95715287188.us-central1.run.app/health" \
+-H "Authorization: bearer $(gcloud auth print-identity-token)" \
+-H "Content-Type: application/json" \
+-d '{"limit": "10"}'
