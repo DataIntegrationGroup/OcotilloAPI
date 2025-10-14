5a350f073ab021a4c352827a3abb3539b26b5e12
jakeross <jirhiker@gmail.com>
2025-10-14 12:07:18 -0600

    feat: replace cfgv with pydantic for validation in water levels transducer transfer

--- transfers/waterlevels_transducer_transfer.py
+++ transfers/waterlevels_transducer_transfer.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
-from cfgv import ValidationError
+from pydantic import ValidationError
 
 from db import Parameter, Thing, Deployment
 from db.transducer import TransducerObservation, TransducerObservationBlock
