f5a71a5d467b8ed2829375341609de73588b8b87
jakeross <jirhiker@gmail.com>
2025-11-18 14:47:44 -0700

    refactor: move lexicon and parameter initialization outside of database setup

--- core/initializers.py
+++ core/initializers.py
@@ -64,8 +64,8 @@ def erase_and_rebuild_db():
         Base.metadata.drop_all(session.bind)
         Base.metadata.create_all(session.bind)
 
-        init_lexicon()
-        init_parameter()
+    init_lexicon()
+    init_parameter()
 
 
 def init_lexicon(path: str = None) -> None:
