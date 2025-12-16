39b51cc383748cff4ae8179762a7435874fccfab
jacob-a-brown <brown.jacob.alexander@gmail.com>
2025-12-16 11:31:58 -0700

    fix: remove outdated variable from testing env

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -503,8 +503,6 @@ def before_all(context):
     context.objects = {}
 
     rebuild = True
-    # rebuild = True
-    erase_data = False
     if rebuild:
         erase_and_rebuild_db()
     elif get_bool_env("ERASE_DATA", False):
