c84af04aabfea374a8ec8850932b8fefc345907e
jirhiker <jirhiker@users.noreply.github.com>
2026-01-18 05:23:35 +0000

    Formatting changes

--- transfers/util.py
+++ transfers/util.py
@@ -62,9 +62,8 @@ class MeasuringPointEstimator:
         df = read_csv("WaterLevels")
         df["DateMeasured"] = pd.to_datetime(df["DateMeasured"], errors="coerce")
         df = df.dropna(subset=["DateMeasured"])
-        self._grouped = (
-            df.groupby("PointID", sort=False)
-            .apply(lambda g: g.sort_values("DateMeasured"))
+        self._grouped = df.groupby("PointID", sort=False).apply(
+            lambda g: g.sort_values("DateMeasured")
         )
         self.verbose = False
 
