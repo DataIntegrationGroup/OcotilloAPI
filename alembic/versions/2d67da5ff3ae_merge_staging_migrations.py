1f02e922862609c9e6ca4a40572c91404127bfa5
kbighorse <kbighorse@users.noreply.github.com>
2026-01-21 20:38:40 +0000

    Formatting changes

--- alembic/versions/2d67da5ff3ae_merge_staging_migrations.py
+++ alembic/versions/2d67da5ff3ae_merge_staging_migrations.py
@@ -5,6 +5,7 @@ Revises: 1d2c3b4a5e67, g4a5b6c7d8e9
 Create Date: 2026-01-21 12:24:14.992587
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,10 +13,9 @@ import geoalchemy2
 import sqlalchemy as sa
 import sqlalchemy_utils
 
-
 # revision identifiers, used by Alembic.
-revision: str = '2d67da5ff3ae'
-down_revision: Union[str, Sequence[str], None] = ('1d2c3b4a5e67', 'g4a5b6c7d8e9')
+revision: str = "2d67da5ff3ae"
+down_revision: Union[str, Sequence[str], None] = ("1d2c3b4a5e67", "g4a5b6c7d8e9")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
