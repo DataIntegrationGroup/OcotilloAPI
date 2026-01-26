324b89f4b27a2765086a7bda98f45639d92b8ece
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2026-01-26 15:46:08 -0600

    [surface_water] Update admin panel to be read-only

--- admin/views/surface_water.py
+++ admin/views/surface_water.py
@@ -1,3 +1,18 @@
+# ===============================================================================
+# Copyright 2025
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
 """
 SurfaceWaterDataAdmin view for NMSampleLocations.
 """
@@ -62,3 +77,20 @@ class SurfaceWaterDataAdmin(OcotilloModelView):
         "source_notes",
         "data_source",
     ]
+
+    # ========== READ ONLY ==========
+    enable_publish_actions = (
+        False  # hides publish/unpublish actions inherited from base
+    )
+
+    def can_create(self, request) -> bool:
+        return False
+
+    def can_edit(self, request) -> bool:
+        return False
+
+    def can_delete(self, request) -> bool:
+        return False
+
+
+# ============= EOF =============================================
