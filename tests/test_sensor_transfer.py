3fbd2bbf30e14c2aa608900a4c181e4347e371df
jakeross <jirhiker@gmail.com>
2026-01-30 17:53:38 +1100

    feat: improve data coercion functions for sensor transfer processing

--- tests/test_sensor_transfer.py
+++ tests/test_sensor_transfer.py
@@ -1,7 +1,6 @@
+from transfers.sensor_transfer import _coerce_wi_mic_gain, _coerce_wi_int
 import numpy as np
 import pandas as pd
-
-from transfers.sensor_transfer import _coerce_wi_mic_gain
 
 
 def test_coerce_wi_mic_gain_numeric():
@@ -22,3 +21,22 @@ def test_coerce_wi_mic_gain_handles_none_like():
     assert _coerce_wi_mic_gain("  ") is None
     assert _coerce_wi_mic_gain(pd.NA) is None
     assert _coerce_wi_mic_gain(np.nan) is None
+
+
+def test_coerce_wi_int_numeric():
+    assert _coerce_wi_int(1) == 1
+    assert _coerce_wi_int(1.9) == 1
+    assert _coerce_wi_int(0.0) == 0
+
+
+def test_coerce_wi_int_strings():
+    assert _coerce_wi_int("2") == 2
+    assert _coerce_wi_int(" 3.0 ") == 3
+    assert _coerce_wi_int("true") is None
+
+
+def test_coerce_wi_int_none_like():
+    assert _coerce_wi_int(None) is None
+    assert _coerce_wi_int("  ") is None
+    assert _coerce_wi_int(pd.NA) is None
+    assert _coerce_wi_int(np.nan) is None
