17d5c82421ca19c8cc3a2da136f33599af5492db
kbighorse <kbighorse@users.noreply.github.com>
2026-01-30 20:02:17 +0000

    Formatting changes

--- alembic/versions/43bc34504ee6_merge_migrations_after_staging_merge.py
+++ alembic/versions/43bc34504ee6_merge_migrations_after_staging_merge.py
@@ -5,6 +5,7 @@ Revises: 3cb924ca51fd, e123456789ab
 Create Date: 2026-01-30 11:52:41.932306
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,10 +13,9 @@ import geoalchemy2
 import sqlalchemy as sa
 import sqlalchemy_utils
 
-
 # revision identifiers, used by Alembic.
-revision: str = '43bc34504ee6'
-down_revision: Union[str, Sequence[str], None] = ('3cb924ca51fd', 'e123456789ab')
+revision: str = "43bc34504ee6"
+down_revision: Union[str, Sequence[str], None] = ("3cb924ca51fd", "e123456789ab")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
