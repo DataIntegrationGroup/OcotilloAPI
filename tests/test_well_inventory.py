b6e5d800433de1d630e5c014e3766bf8b5b72c36
jacob-a-brown <brown.jacob.alexander@gmail.com>
2026-03-18 15:36:59 -0600

    fix(test): make docstring more accurate

--- tests/test_well_inventory.py
+++ tests/test_well_inventory.py
@@ -530,7 +530,7 @@ def test_well_inventory_db_contents_with_waterlevels(tmp_path):
 
 
 def test_measuring_point_height_ft_used_for_thing_and_observation(tmp_path):
-    """When both mp_height and measuring_point_height_ft are provided, measuring_point_height_ft should be used for the thing's and observation's measuring_point_height."""
+    """When measuring_point_height_ft is provided it is used for the thing's and observation's measuring_point_height."""
     row = _minimal_valid_well_inventory_row()
     row.update(
         {
