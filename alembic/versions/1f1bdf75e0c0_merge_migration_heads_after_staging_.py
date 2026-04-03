6f6f0989b91c5489e7f879d54a16857bdad52cb4
kbighorse <93388+kbighorse@users.noreply.github.com>
2026-04-03 17:23:35 +0000

    Formatting changes

--- alembic/versions/1f1bdf75e0c0_merge_migration_heads_after_staging_.py
+++ alembic/versions/1f1bdf75e0c0_merge_migration_heads_after_staging_.py
@@ -5,6 +5,7 @@ Revises: 545a5b77e5e8, t6u7v8w9x0y1
 Create Date: 2026-04-03 10:12:48.253856
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,10 +13,9 @@ import geoalchemy2
 import sqlalchemy as sa
 import sqlalchemy_utils
 
-
 # revision identifiers, used by Alembic.
-revision: str = '1f1bdf75e0c0'
-down_revision: Union[str, Sequence[str], None] = ('545a5b77e5e8', 't6u7v8w9x0y1')
+revision: str = "1f1bdf75e0c0"
+down_revision: Union[str, Sequence[str], None] = ("545a5b77e5e8", "t6u7v8w9x0y1")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
